4d461cc4de1867ccbe1a833caa44cbb13cfdfaac
tdstudiotishandilshad-arch <tdstudiotishandilshad@gmail.com>
2025-12-11 15:20:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=7hBXmbrK#autd1Rrys1M4LTocdstyZO8RpxQH_v7YUuP2eeXEInk' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
